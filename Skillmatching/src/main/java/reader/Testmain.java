08982a97ddb888190cfccf6dc4122c034e86be85
konierik <ErikPaul.Konietzko@ipk.fraunhofer.de>
2021-07-03 22:32:40 +0200

    tested parsePointer()&replaceToIdent() with more array markes - works

--- Skillmatching/src/main/java/reader/Testmain.java
+++ Skillmatching/src/main/java/reader/Testmain.java
@@ -8,30 +8,33 @@ import java.util.List;
 public class Testmain {
 	public static void main (String[] args) throws IOException {
 		JSONReader readie = new JSONReader();
-		String input="/data/projects/result/edges/~/node/contributors/edges/~/node/id";
-		String ident="/data/projects/result/edges/~/node/id";
+		String input="/data/profiles/result/edges/~/node/followingProjects/edges/~/node/tags/~/name";//"/data/projects/result/edges/~/node/contributors/edges/~/node/id";
+		String ident="/data/profiles/result/edges/~/node/followingProjects/edges/~/node/id";//"/data/projects/result/edges/~/node/id";
 		String output="/data/projects/result/edges/25/node/contributors/edges/~/node/id";
-		readie.setFile("C://Springboot-Repository//Skillmatch//Skillmatching//src//main//resources//inputdata//sampledata_projects.json");
+		readie.setFile("C://Users//konierik//Nextcloud2//MA-Arbeit//03_Ontology//Skills//WIF_data//sampledata_user.json");
 		readie.open();
+		System.out.println(readie.getStructure().toString());
 		ArrayList<ArrayList<String>> jsondata=readie.parsePointer(input);
 		System.out.println(jsondata.toString());
 		System.out.println(jsondata.size());
 		System.out.println(jsondata.get(0).size());
+		
 		for (int i=0;i<jsondata.get(0).size();i++) {
 			
 				System.out.println("Key: "+jsondata.get(0).get(i));
 				System.out.println("Value: "+jsondata.get(1).get(i)+"\n\n");
 			
 		}
-		
+		/*
 		ArrayList<ArrayList<String>> jsondatadomain=readie.replaceToIdent(jsondata, ident);
 		System.out.println(jsondatadomain.toString());
+		System.out.println("Länge "+jsondatadomain.get(0).size());
 		for (int i=0;i<jsondatadomain.get(0).size();i++) {
 			
 			System.out.println("Key: "+jsondatadomain.get(0).get(i));
 			System.out.println("Value: "+jsondatadomain.get(1).get(i)+"\n\n");
 		
-	}
+		}
 		
 		
 		/*String clean = output.replaceAll("\\D+",""); //remove non-digits
