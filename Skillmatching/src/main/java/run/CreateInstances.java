340f207662666f942c383a05f54ce6ab74472f7e
konierik <ErikPaul.Konietzko@ipk.fraunhofer.de>
2021-07-14 11:15:48 +0200

    Divided main method in two parts

---
+++ Skillmatching/src/main/java/run/CreateInstances.java
@@ -0,0 +1,202 @@
+package run;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import process.JSON2NTmapper;
+import process.NTParser;
+import process.OntoModeler;
+import reader.JSONReader;
+
+public class CreateInstances {
+
+	public static String skillIRI="https://github.com/konierik/Skillmatching/raw/main/Skillmatching/data/on_skills.owl";
+	
+	public static void main (String[] args) throws IOException {
+		/*A mapping OntoModeler is created to instantiate Project 
+		 * and User data for the OSHPDP ontology, that represents a project landscape for OSH projects. 
+		 * In this case the OSHPDP ontology itself contains the mapping annotations. A mapping annotation is 
+		 * a json pointer representing where to find the information in a json formatted file. The annotations in
+		 * the OSHPDP ontology is based on the graphQL api schema structure of the projectpartner Wikifactory (https://wikifactory.com/api/graphql)
+		 * but can be changed to any json format. A classmapping represents a pointer which information 
+		 * will be than instantiated as a OWLClass of the OSHPD. A Objectpropertymapping pointer will be instantiated
+		 * as the relating object property (same for datapropertymapping) and a identifiermapping represents the domain
+		 * of the relating property. 
+		 * As example: An object property is annotated with an objectpropertymapping-annotation. 
+		 * The value behind the pointer in the annotation is instantiated as range instance of the object property.
+		 * Additionally to data-/objectpropertymappings there is a identifier mapping, which contains the pointer to the
+		 * respective domain class information for the property.*/
+		
+		//////////////////////////////////////////////////////////////////////////////////////////////////////////
+		//
+		//			Mapping Ontology
+		//
+		/////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
+		// Creating an OntoModeler for the ontology holding the mappings
+		OntoModeler mapping=new OntoModeler();
+		//Setting the IRI of the ontology
+		mapping.setIRI("https://github.com/konierik/Skillmatching/raw/main/Skillmatching/data/on_OSHPDP_schema.owl");
+		//Loading the ontology, here: from web
+		mapping.loadOnto();
+		//merge the skill ontology into the Modeler, so every class, property and instance is accessible
+		mapping.mergeOntology(skillIRI);
+		//annotation arrays for later instaniation
+		ArrayList<ArrayList<String>> classannotations=null;
+		ArrayList<ArrayList<String>> dataannotations = null;
+		ArrayList<ArrayList<String>> objectannotations = null;
+		
+		//////////////////////////////////////////////////////////////////////////////////////////////////////////
+		//
+		//			variables for instance ontology
+		//
+		/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		String instanceIRI="https://github.com/konierik/Skillmatching/raw/main/Skillmatching/data/on_Instances.ttl";
+		
+		//////////////////////////////////////////////////////////////////////////////////////////////////////////
+		//
+		//			setup json reader
+		//
+		/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		//create one JSONReader per data file: issues, projects, user
+		JSONReader readIssues=new JSONReader();
+		//set the file to read and open it
+		readIssues.setFile("C://Springboot-Repository//Skillmatch//Skillmatching//data//sampledata_issues_anonym.json");
+		readIssues.open();
+		
+		JSONReader readUser=new JSONReader();
+		//set the file to read and open it
+		readUser.setFile("C://Springboot-Repository//Skillmatch//Skillmatching//data//sampledata_user_anonym.json");
+		readUser.open();
+		
+		JSONReader readProjects=new JSONReader();
+		//set the file to read and open it
+		readProjects.setFile("C://Springboot-Repository//Skillmatch//Skillmatching//data//sampledata_projects_anonym.json");
+		readProjects.open();
+		
+		//////////////////////////////////////////////////////////////////////////////////////////////////////////
+		//
+		//			setup json2ntmapper
+		//
+		/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		JSON2NTmapper ntmapper=new JSON2NTmapper();
+		//setting iris for instantiation
+		ntmapper.setInstanceIRI(instanceIRI);
+		ntmapper.setMappingIRI(mapping.getIRI().toString());
+		
+		
+		//////////////////////////////////////////////////////////////////////////////////////////////////////////
+		//
+		//			Instantiate issue-mappings
+		//
+		/////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
+		//going through all 5 issue_mapping_annotations
+		System.out.println("Instantiate Issue-mappings");
+		for (int i=1; i<=5;i++) { 
+			/*Define what mapping annotations should be looked for (in case there are more mappings in the ontology).
+			 * Mappings can be named differently if the ontology is used to map several sources of different structure.*/
+			mapping.setClassmapping("wif_issue_"+i+"_cmap");
+			//mapping.setClassIdent("identifier");
+			mapping.setObjectpropertymapping("wif_issue_"+i+"_opmap");
+			mapping.setDatapropertymapping("wif_issue_"+i+"_dpmap");
+			
+			// Extract the pointers from the mapping annotations and iris of the respecting concepts as lists
+			classannotations = mapping.getClassesAnnotations(); //Format: [[classmapping pointer][rdfsType][classIRI]]
+			dataannotations = mapping.getDatapropertiesAnnotations(); //Format: dataproertyIRI|datapropertymapping pointer
+			objectannotations = mapping.getObjectpropertiesAnnotations(); //Format: objectproperty IRI|objectpropertymapping pointer
+				
+			ntmapper.instantiateToNTClasses(classannotations, readIssues );
+			ntmapper.instantiateToNTDataproperties(dataannotations, readIssues);
+			ntmapper.instantiateToNTObjectproperties(objectannotations, readIssues);
+		}
+		System.out.println("\n\n");
+		
+		//////////////////////////////////////////////////////////////////////////////////////////////////////////
+		//
+		//			Instantiate user-mappings
+		//
+		/////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
+		//going through all 6 user_mapping_annotations
+		System.out.println("Instantiate User-mappings");
+		for (int i=1; i<=5;i++) { 
+			/*Define what mapping annotations should be looked for (in case there are more mappings in the ontology).
+			 * Mappings can be named differently if the ontology is used to map several sources of different structure.*/
+			mapping.setClassmapping("wif_user_"+i+"_cmap");
+			//mapping.setClassIdent("identifier");
+			mapping.setObjectpropertymapping("wif_user_"+i+"_opmap");
+			mapping.setDatapropertymapping("wif_user_"+i+"_dpmap");
+			
+			// Extract the pointers from the mapping annotations and iris of the respecting concepts as lists
+			classannotations = mapping.getClassesAnnotations(); //Format: [[classmapping pointer][rdfsType][classIRI]]
+			dataannotations = mapping.getDatapropertiesAnnotations(); //Format: dataproertyIRI|datapropertymapping pointer
+			objectannotations = mapping.getObjectpropertiesAnnotations(); //Format: objectproperty IRI|objectpropertymapping pointer
+				
+			ntmapper.instantiateToNTClasses(classannotations, readUser );
+			ntmapper.instantiateToNTDataproperties(dataannotations, readUser);
+			ntmapper.instantiateToNTObjectproperties(objectannotations, readUser);
+		}
+		System.out.println("\n\n");
+		
+		//////////////////////////////////////////////////////////////////////////////////////////////////////////
+		//
+		//			Instantiate prject-mappings
+		//
+		/////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
+		//going through all 3 project_mapping_annotations
+		System.out.println("Instantiate User-mappings");
+		for (int i=1; i<=3;i++) { 
+			/*Define what mapping annotations should be looked for (in case there are more mappings in the ontology).
+			 * Mappings can be named differently if the ontology is used to map several sources of different structure.*/
+			mapping.setClassmapping("wif_project_"+i+"_cmap");
+			//mapping.setClassIdent("identifier");
+			mapping.setObjectpropertymapping("wif_project_"+i+"_opmap");
+			mapping.setDatapropertymapping("wif_project_"+i+"_dpmap");
+			
+			// Extract the pointers from the mapping annotations and iris of the respecting concepts as lists
+			classannotations = mapping.getClassesAnnotations(); //Format: [[classmapping pointer][rdfsType][classIRI]]
+			dataannotations = mapping.getDatapropertiesAnnotations(); //Format: dataproertyIRI|datapropertymapping pointer
+			objectannotations = mapping.getObjectpropertiesAnnotations(); //Format: objectproperty IRI|objectpropertymapping pointer
+				
+			ntmapper.instantiateToNTClasses(classannotations, readProjects );
+			ntmapper.instantiateToNTDataproperties(dataannotations, readProjects);
+			ntmapper.instantiateToNTObjectproperties(objectannotations, readProjects);
+		}
+		System.out.println("\n\n");
+		
+		readIssues.close();
+		readUser.close();
+		readProjects.close();
+		//////////////////////////////////////////////////////////////////////////////////////////////////////////
+		//
+		//			parse nt file, adding prefixes and import statements and converting to ttl format
+		//
+		/////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+		
+		//Set output string for file
+		String ntoutput="C://Springboot-Repository//Skillmatch//Skillmatching//data//on_Instances.nt";
+		ntmapper.setNToutputLocation(ntoutput);
+		ntmapper.toNTFile();
+		NTParser ntparse=new NTParser(ntoutput);
+		ntparse.setPrefix("", instanceIRI);
+		ntparse.setPrefix("oshpd", mapping.getIRIString());
+		ntparse.setPrefix("skills", skillIRI);
+		ntparse.setPrefix("owl", "http://www.w3.org/2002/07/owl");
+		ntparse.readNTModel();
+		ntparse.setOntologyIRI(instanceIRI);
+		ntparse.addImport(mapping.getIRIString());
+		ntparse.addImport("https://github.com/konierik/Skillmatching/raw/main/Skillmatching/data/on_skills.owl");
+		ntparse.setOutput("C://Springboot-Repository//Skillmatch//Skillmatching//data//on_Instances.ttl");
+		ntparse.parseNT(instanceIRI);
+		
+		
+
+	
+	}
+}
---
+++ Skillmatching/src/main/java/run/CreateSkill.java
@@ -0,0 +1,70 @@
+package run;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+
+import reader.SkillReader;
+
+public class CreateSkill {
+	public static void main(String args[]) throws OWLOntologyCreationException, IOException {
+		//initialization of variables used:
+		String projectdir=System.getProperty("user.dir").replace("\\", "/");
+		
+		
+		/*At first the skill ontology is instantiated. For this a JSONReader is created that extracts
+		the input data from a json formated file. Than an OntoModeler is used to instantiate the data into
+		the semantic net.*/
+		
+		////////////////////////////////////////////////////////////////////////////////
+		//
+		//					Skill ontology variables
+		//
+		////////////////////////////////////////////////////////////////////////////////
+		String skill_inputfilelocation=projectdir+"/src/main/resources/inputdata/skills_schema.json";
+		String skill_ontolocation=projectdir+"/src/main/resources/inputdata/on_skills_void.owl";
+		String skill_ontofilelocation=projectdir+"/src/main/resources/ontology/on_skills_classes.owl";
+		String skill_prefix="https://github.com/OPEN-NEXT/WP3_Skillmatching/raw/main/ontology/on_skills.owl";
+		String skillIRI="https://github.com/konierik/Skillmatching/raw/main/Skillmatching/data/on_skills.owl";
+
+				
+		////////////////////////////////////////////////////////////////////////////////
+		//
+		//					Skill ontology instantiation
+		//
+		////////////////////////////////////////////////////////////////////////////////
+		
+		//The skill ontology is already build with classes and relations.
+		//The application case data for the skills to use as instances is provided in a separate json file.
+		
+
+		//first create instances for the skill ontology relating on the skill inputs
+		SkillReader skully= new SkillReader();
+		//setting the location of the json input file
+		skully.setFileLocation(skill_inputfilelocation);
+		//create a reader to read the set file
+		try {
+			skully.createReader();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		//create a pointer to the section in the json input file we are looking at. Here we want to go into the skill_target section
+		skully.setPointer("/skill_targets");
+		//setting the keys for targets, entity_types, and skill_actions. 
+		//Here they have the same naming, but in other cases choose the equivalent keys
+		skully.setSkill_target("skill_target");
+		skully.setEntity_type("entity_type");
+		//We do not have to set the skill_action, because they are used as objectproperties (relation between classes) in the ontology. The architecture is already build.
+		//Set ontologylocation of the void ontology to instantiate. 
+		skully.setOntoLocation(skill_ontolocation);
+		//set prefix for the instances
+		skully.setSkillPrefix(skill_prefix);
+		//instantiate the set targets as instances of a class relating to the respective entity_types
+		skully.instantiateTargetsAsClass();
+		//save instantiated ontology. 
+		skully.saveOntology(skill_ontofilelocation);
+		
+
+	}
+}
